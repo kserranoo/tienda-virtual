ec7cf7a1642962f6c67e4382bd82839267198b43
Kimberlyn Serrano <113371632+kserranoo@users.noreply.github.com>
2022-10-15 22:00:30 -0400

    Se fusionó el carrito de compras
    Se fusionó el carrito de compras, se hizo responsive y se realizó la lógica para esconder el menu mobile cada vez que se abre el carrito de compras mobile y visceversa

--- main.js
+++ main.js
@@ -1,8 +1,43 @@
-const navEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const navEmail = document.getElementsByClassName('navbar-email')[0];
+const desktopMenu = document.getElementsByClassName('desktop-menu')[0];
+const navMobileMenu = document.getElementsByClassName('menu')[0];
+const mobileMenu = document.getElementsByClassName('mobile-menu')[0];
+const navShopping = document.getElementsByClassName('nav-shopping-cart')[1];
+const shoppingCart = document.getElementsByClassName('shopping')[0];
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+navMobileMenu.addEventListener('click', toggleMobileMenu);
+navShopping.addEventListener('click', toggleShopping)
 
 function toggleDesktopMenu() {
+    const isShoppingCartClosed = shoppingCart.classList.contains('inactive');
+
+    if (!isShoppingCartClosed) {
+        shoppingCart.classList.add('inactive')
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu() {
+    const isShoppingCartClosed = shoppingCart.classList.contains('inactive');
+
+    if (!isShoppingCartClosed) {
+        shoppingCart.classList.add('inactive')
+    }
+    
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShopping() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+    }
+
+    shoppingCart.classList.toggle('inactive')
+}
+
+
+
